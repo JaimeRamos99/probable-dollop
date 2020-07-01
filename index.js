0ddec6abf9ee9d47ef9498ffee5afa773ce14897
JaimeRamos99 <jrossetes@uninorte.edu.co>
2020-07-01 02:23:35 -0500

    receive files

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-"use strict";
+'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const path = require('path');
 const router = express.Router();
 const app = express();
 const ctrl = require('./controller');
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads/')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname + '')
-    }
-  });
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, './uploads'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname + '');
+  },
+});
 
-const upload = multer({ storage : storage});
-app.listen(8080, ()=>{
-    console.log('API escuchando en puerto 8080')
+const upload = multer({ storage: storage });
+app.listen(8080, () => {
+  console.log('API escuchando en puerto 8080');
 });
 
 app.use(router);
 app.use(bodyParser.json());
 
-router.post('/server/createContainers', upload.fields([{ name: 'initial', maxCount: 1 },{ name: 'partition', maxCount: 1 }]), async function(req, res){ //receive the graph (.txt's)
-  let r = await ctrl.containers();
-  res.status(200).send('ok');
-});
+router.post(
+  '/server/createContainers',
+  upload.fields([
+    { name: 'initial', maxCount: 1 },
+    { name: 'partition', maxCount: 1 },
+  ]),
+  async function (req, res) {
+    //receive the graph (.txt's)
+    //let r = await ctrl.containers();
+    res.status(200).send('ok');
+  }
+);
